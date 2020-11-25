3cd0e0d0e57a1e018f34a03d29d4d8b270006add
Anna Gavrish <Gavrish@lanit.ru>
2020-11-26 01:16:03 +0300

    ДЗ 8: запрос температуры через API

--- app/src/main/java/ru/geekbrains/bananaforecast/MainActivity.java
+++ app/src/main/java/ru/geekbrains/bananaforecast/MainActivity.java
@@ -5,21 +5,42 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.stream.Collectors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import ru.geekbrains.bananaforecast.model.WeatherRequest;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final boolean DEBUG = false;
     private static final int DAY_COUNT = 5;
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s,RU&appid=%s";
     private City city;
     private static final int SETTING_CODE = 88;
+    private TextView txtViewCity;
+    private TextView txtViewTemperature;
+    private TextView txtViewPressure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +52,69 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         setContentView(R.layout.activity_main);
 
-        Button setting = findViewById(R.id.button3);
-        setting.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivityForResult(intent, SETTING_CODE);
-        });
-
-        initRecyclerView(generateDays(), generateTemperatures());
+        txtViewCity = (TextView) findViewById(R.id.text_city);
+        txtViewTemperature = (TextView) findViewById(R.id.text_curr_temp);
+        txtViewPressure = (TextView) findViewById(R.id.text_pressure);
 
         Intent intent = getIntent();
         if (intent.hasExtra("parcel")) {
             city = (City) intent.getSerializableExtra("parcel");
         } else {
-            city = new City(getResources().getStringArray(R.array.cities)[0], getResources().getStringArray(R.array.temperatures)[0],
-                getResources().getStringArray(R.array.pressures)[0], getResources().getStringArray(R.array.windSpeeds)[0]);
+            city = new City(getResources().getStringArray(R.array.cities)[0]);
         }
 
-        TextView txtViewCity = (TextView) findViewById(R.id.text_city);
+        final URL uri;
+        try {
+            uri = new URL(String.format(WEATHER_URL, URLEncoder.encode(city.getName(), StandardCharsets.UTF_8.toString()), BuildConfig.WEATHER_API_KEY));
+
+            final Handler handler = new Handler();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    HttpsURLConnection urlConnection = null;
+                    try {
+                        urlConnection = (HttpsURLConnection) uri.openConnection();
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.setReadTimeout(10000);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        String result = getLines(in);
+                        Gson gson = new Gson();
+                        final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                displayWeather(weatherRequest);
+                            }
+                        });
+                    } catch (Exception e) {
+                        Log.e(TAG, "Fail connection", e);
+                        e.printStackTrace();
+                    } finally {
+                        if (null != urlConnection) {
+                            urlConnection.disconnect();
+                        }
+                    }
+                }
+            }).start();
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Fail URI", e);
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "Fail encode URL", e);
+            e.printStackTrace();
+        }
+
         txtViewCity.setText(city.getName());
-        TextView txtViewTemperature = (TextView) findViewById(R.id.text_curr_temp);
         txtViewTemperature.setText(city.getTemperature());
+        txtViewPressure.setText(city.getPressure());
 
-        if (city.isNeedPressure()) {
-            TextView txtViewLabelPressure = (TextView) findViewById(R.id.label_pressure);
-            TextView txtViewPressure = (TextView) findViewById(R.id.text_pressure);
-            txtViewPressure.setText(city.getPressure());
-            txtViewLabelPressure.setVisibility(View.VISIBLE);
-            txtViewPressure.setVisibility(View.VISIBLE);
-        }
+        Button setting = findViewById(R.id.button3);
+        setting.setOnClickListener(v -> {
+            Intent intent1 = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivityForResult(intent1, SETTING_CODE);
+        });
+
+        initRecyclerView(generateDays(), generateTemperatures());
 
         Button btnSelectCity = (Button) findViewById(R.id.buttonSelectCity);
         btnSelectCity.setOnClickListener(this);
@@ -179,9 +235,25 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SETTING_CODE){
+        if (requestCode == SETTING_CODE) {
             recreate();
         }
     }
 
+    private String getLines(BufferedReader in) {
+        return in.lines().collect(Collectors.joining("\n"));
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void displayWeather(WeatherRequest weatherRequest) {
+        txtViewCity.setText(weatherRequest.getName());
+        txtViewTemperature.setText(String.format("%.0f", weatherRequest.getMain().getTemp() - 273.15));
+        txtViewPressure.setText(String.format("%.0f", weatherRequest.getMain().getPressure()/1.333));
+
+        if (city.isNeedPressure()) {
+            TextView txtViewLabelPressure = (TextView) findViewById(R.id.label_pressure);
+            txtViewLabelPressure.setVisibility(View.VISIBLE);
+            txtViewPressure.setVisibility(View.VISIBLE);
+        }
+    }
 }
